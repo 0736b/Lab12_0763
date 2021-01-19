0b0ee8fad1ae4ba957d6117a4c52f8ae4570dbd6
0736b <thanawat_bumpengpun@elearning.cmu.ac.th>
2021-01-19 16:20:25 +0700

    lab12_2.cpp completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,42 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool img[][M]){
+    cout << "------------------------------------------------------------------------"<< endl;
+    for(int i = 0 ; i < 30 ; i++){
+        cout << "|";
+        for(int j = 0 ; j< 70; j++){
+            if(img[i][j] == 1){
+            cout << "*";
+            }
+            else{
+                cout<< " ";
+            }
+        }
+        cout << "|";
+        cout << endl;
+    }
+    cout << "------------------------------------------------------------------------"<< endl;
+}
+
+void updateImage(bool paint[][M],int size,int x,int y){
+
+double radius;
+
+if(size == 1){
+    paint[x][y] = 1;
+}
+else if(size > 1){
+   for(int i = 0 ; i < 30 ; i++){
+       for(int j = 0; j<70; j++){
+           radius = pow((i-x),2) + pow((j-y),2);
+           radius = sqrt(radius);
+           if(radius <= size-1){
+               paint[i][j] = 1;
+           }
+       }
+   }
+   } 
+}
+
