0f94e15418d42744143f04cd344c20e0b1541824
0736b <thanawat_bumpengpun@elearning.cmu.ac.th>
2021-01-19 15:42:29 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,30 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double Array[], int size, double Ans[]){
+    double sum = 0,min,max,sumexpo = 0,sumgeo = 1, sumharmonic = 0,std;
+    max = Array[0];
+    min = Array[0];
+
+    for(int i = 0; i< size ; i++){
+        sum+=Array[i];
+        sumexpo += pow(Array[i],2);
+        sumgeo = sumgeo*Array[i];
+        sumharmonic = sumharmonic + (1/Array[i]);
+        if(max < Array[i]){
+            max = Array[i];
+        }
+        if(min > Array[i]){
+            min = Array[i];
+        }
+    }
+
+    std = (sumexpo/size) - pow((sum/size),2);
+    Ans[0] = sum/size;  //Arithmetric Mean
+    Ans[1] = sqrt(std);  //Standard Deviation
+    Ans[2] = pow(sumgeo,(1.0/size)); // Geometric Mean
+    Ans[3] = size/sumharmonic; // Harmonic Mean
+    Ans[4] = max;
+    Ans[5] = min;
+}
